9417b6b70308eb91aeb6c310089fc676b62af149
Corentin Le Bail <corentin@deb-co.speedinfo.biz>
2017-03-20 15:16:10 +0100

    tileset in 32x32

--- common.h
+++ common.h
@@ -2,10 +2,10 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 //----------------------------------------------------------------------------
-#define TILE_WIDTH                  16
-#define TILE_HEIGHT                 16
-#define SEP_WIDTH                   2
-#define SEP_HEIGHT                  2
+#define TILE_WIDTH                  32
+#define TILE_HEIGHT                 32
+#define SEP_WIDTH                   4
+#define SEP_HEIGHT                  4
 #define REAL_TILE_WIDTH             (TILE_WIDTH + SEP_WIDTH)
 #define REAL_TILE_HEIGHT            (TILE_HEIGHT + SEP_HEIGHT)
 #define OFFSET_X                    (2 * SEP_WIDTH)
