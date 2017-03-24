f028ae4bf17f094559a87b148fe761ed7be0e1cd
Corentin Le Bail <corentin@deb-co.speedinfo.biz>
2017-03-24 14:39:11 +0100

    levelEdit CLevel QList de / serialize

--- CLevel.cpp
+++ CLevel.cpp
@@ -5,8 +5,13 @@ CLevel::CLevel() {
 }
 //----------------------------------------------------------------------------
 QDataStream& operator<<(QDataStream& out, const CLevel& level) {
+	int i;
+
     out << level.playerStartPos;
-    out << level.monsterStartPoss;
+    out << level.monsterStartPoss.size();
+    for(i=0;i<level.monsterStartPoss.size();i++) {
+    	out << level.monsterStartPoss.at(i);
+   	}
     out << level.front;
     out << level.back;
 
@@ -14,8 +19,16 @@ QDataStream& operator<<(QDataStream& out, const CLevel& level) {
 }
 //-----------------------------------------------------------------------------------------------
 QDataStream& operator>>(QDataStream& in, CLevel& level) {
+	int nbMonster, i;
+
     in >> level.playerStartPos;
-    in >> level.monsterStartPoss;
+    in >> nbMonster;
+    for(i=0;i<nbMonster;i++) {
+    	QPoint mp;
+    
+    	in >> mp;
+    	level.monsterStartPoss.append(mp);
+   	}
     in >> level.front;
     in >> level.back;
 
